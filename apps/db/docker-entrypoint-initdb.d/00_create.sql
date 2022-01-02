9a8e3fd332283b5b674400027b7d084030245b57
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-02 15:38:20 +0900

    feat: DB環境構築時にcustomersテーブルが作られるように変更3

--- apps/db/docker-entrypoint-initdb.d/00_create.sql
+++ apps/db/docker-entrypoint-initdb.d/00_create.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS customers;
 
 CREATE TABLE customers
 (
-    `id`    INT NOT NULL AUTO_INCREMENT,
-    `name`  VARCHAR(191),
-    `email` VARCHAR(191),
-    `created_at` timestamp,
-    `updated_at` timestamp,
+    `id`         INT       NOT NULL AUTO_INCREMENT,
+    `name`       VARCHAR(191),
+    `email`      VARCHAR(191),
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
 
