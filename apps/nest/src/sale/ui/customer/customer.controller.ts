7838a9666de22aa5b90eeaf3491954f3a0f31bb7
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-03 12:31:49 +0900

    feat: GetAPIのレスポンスを実装

--- apps/nest/src/sale/ui/customer/customer.controller.ts
+++ apps/nest/src/sale/ui/customer/customer.controller.ts
@@ -1,14 +1,26 @@
 import { Controller, Get, Inject, Post, Query } from '@nestjs/common';
 import { QueryParameters } from './queryParameters';
 import { CustomerService } from '../../app/service/customer/customerService';
+import { Customer } from '../../domain/model/customer';
+
+type ResponseSchema<T> = {
+  isSuccess: boolean;
+  message: string;
+  body: T;
+};
 
 @Controller('customer')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
   @Get()
-  async find(@Query() query: QueryParameters) {
-    console.log(await this.customerService.find(query));
+  async find(@Query() query: QueryParameters): Promise<ResponseSchema<Customer>> {
+    const customer = await this.customerService.find(query);
+    return {
+      isSuccess: true,
+      message: '',
+      body: customer,
+    };
   }
 
   @Post()
