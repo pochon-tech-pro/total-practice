c8cd016b848602d56424cfd760b127cc096891f3
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-07 19:44:49 +0900

    feat: Reactを思い出す。

--- apps/react-app/src/components/App.tsx
+++ apps/react-app/src/components/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import CustomerList from "./CustomerList";
+import {Customer} from "../type/customer";
+
+const sample: Customer[] = [
+    {name: 'userA', tel: '03-0001-0001'},
+    {name: 'userB', tel: '03-0002-0002'}
+]
 
 const App: React.VFC = () => {
     return (
         <React.Fragment>
-            <div>Hello React TypeScript</div>
+            <CustomerList customers={sample}/>
         </React.Fragment>
     );
 }
---
+++ apps/react-app/src/components/CustomerList.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {Customer} from "../type/customer";
+
+interface Props {
+    customers: Customer[]
+}
+
+const CustomerList: React.VFC<Props> = props => {
+    const {customers} = props;
+
+    return (
+        <React.Fragment>
+            <h4>顧客一覧</h4>
+            <table>
+                <thead>
+                <tr>
+                    <th>顧客名</th>
+                    <th>TEL</th>
+                </tr>
+                </thead>
+                <tbody>
+                {customers.map(customer => {
+                    return (
+                        <tr>
+                            <td>{customer.name}</td>
+                            <td>{customer.tel}</td>
+                        </tr>
+                    );
+                })}
+                </tbody>
+            </table>
+
+        </React.Fragment>
+    );
+}
+
+export default CustomerList;
