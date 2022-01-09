39a54c12d15735cc9b1567e86726435ac6c25aeb
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-10 00:46:48 +0900

    feat: CORS許可のOriginをenvから参照するように変更

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8000',
+    origin: process.env.FRONTEND_URI,
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   });
   await app.listen(3000);
