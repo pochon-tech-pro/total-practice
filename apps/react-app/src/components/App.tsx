8c5ea669907d9cd7efad0b18db7e8e793c3caccb
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-10 15:34:46 +0900

    feat: useEffectでAPIの呼び出しを実装してみる

--- apps/react-app/src/components/App.tsx
+++ apps/react-app/src/components/App.tsx
@@ -1,10 +1,24 @@
-import React, {useReducer} from "react";
+import React, {useEffect, useReducer} from "react";
 import CustomerForm from "./CustomerForm";
 import CustomerRows from "./CustomerRows";
 import reducer from "../reducers";
 
+const getCustomerAPI = async () => {
+    try {
+        const res = await fetch("http://localhost:3000/customer?tel=03-1111-2222");
+        const data = await res.json();
+        console.log(data);
+    } catch (e) {
+        console.log(e)
+    }
+}
+
 const App: React.FC = () => {
     const [state, dispatch] = useReducer(reducer, []);
+
+    useEffect(() => {
+        (async () => await getCustomerAPI())();
+    }, []);
 
     return (
         <div className={"container"}>
