464510f08e34522e00099e83c7fbf9982883abd5
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-12 10:33:44 +0900

    refactor: APIにエラーハンドリング追加

--- apps/react-app/src/api/index.ts
+++ apps/react-app/src/api/index.ts
@@ -1,29 +1,39 @@
 import {Customer} from "../type/customer";
 
 export const getCustomerAPI = async (): Promise<Customer[]> => {
-    const res = await fetch("http://localhost:3000/customer/all");
-    const data = await res.json() as {
-        isSuccess: boolean
-        message: string
-        body: { id: number; name: string; tel: string; }[]
-    };
-    return data.body.map(item => ({
-        name: item.name,
-        tel: item.tel
-    }));
+    try {
+        const res = await fetch("http://localhost:3000/customer/all");
+        const data = await res.json() as {
+            isSuccess: boolean
+            message: string
+            body: { id: number; name: string; tel: string; }[]
+        };
+        return data.body.map(item => ({
+            name: item.name,
+            tel: item.tel
+        }));
+    } catch (e) {
+        console.log('不明なエラーが発生しています', e);
+        return [];
+    }
 }
 
-export const postCustomerAPI = async ({name, tel}: { name: string, tel: string }): Promise<boolean> => {
-    const res = await fetch("http://localhost:3000/customer/", {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({name, tel}),
-    });
-    const data = await res.json() as {
-        isSuccess: boolean
-        message: string
-        body: string
-    };
-
-    return data.isSuccess;
+export const postCustomerAPI = async ({name, tel}: { name: string, tel: string }): Promise<void> => {
+    try {
+        const res = await fetch("http://localhost:3000/customer/", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name, tel}),
+        });
+        const data = await res.json() as {
+            isSuccess: boolean
+            message: string
+            body: string
+        };
+        if (!data.isSuccess) {
+            alert(data.message);
+        }
+    } catch (e) {
+        console.log('不明なエラーが発生しています', e);
+    }
 }
