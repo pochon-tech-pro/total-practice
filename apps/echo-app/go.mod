6b8783079f9eb91c51896687569b02a4013b2587
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-13 21:09:15 +0900

    feat: gormを試す

--- apps/echo-app/go.mod
+++ apps/echo-app/go.mod
@@ -2,4 +2,8 @@ module echo-app
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.2.2
+require (
+	github.com/labstack/echo/v4 v4.2.2
+	gorm.io/driver/mysql v1.0.6
+	gorm.io/gorm v1.21.9
+)
--- apps/echo-app/main.go
+++ apps/echo-app/main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
+func connect() {
+	db := "db"
+	user := "user"
+	pass := "pass"
+	host := "mysql"
+	port := "3306"
+
+	dsn := user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + db + "?charset=utf8mb4"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	connect()
+	sqlDB, _ := DB.DB()
+	defer sqlDB.Close()
+	err := sqlDB.Ping()
+
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "データベース接続失敗")
+	} else {
+		return c.String(http.StatusOK, "Hello, World!")
+	}
 }
 
 func main() {
