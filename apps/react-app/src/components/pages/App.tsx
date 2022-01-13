a24302ffdd804a9a293dea247abd3ec9d6de5bf8
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-13 09:38:54 +0900

    refactor: Reducerが不要なので消していく。

--- apps/react-app/src/components/pages/App.tsx
+++ apps/react-app/src/components/pages/App.tsx
@@ -2,26 +2,15 @@ import React, {useEffect, useReducer} from "react";
 import CustomerForm from "../organisms/CustomerForm";
 import CustomerRows from "../organisms/CustomerRows";
 import reducer from "../../reducers";
-import {FETCH_CUSTOMER} from "../../actions";
-import {getCustomerAPI} from "../../api";
 import {useAllCustomers} from "../../hooks/useAllCustomers";
 
 const App: React.FC = () => {
     const [state, dispatch] = useReducer(reducer, []);
 
     const { getCustomers, customers } = useAllCustomers();
-
     useEffect(() => {
-        (async () => {
-            const customerList = await getCustomerAPI();
-            dispatch({
-                type: FETCH_CUSTOMER,
-                payload: customerList
-            });
-        })();
         getCustomers();
     }, [getCustomers]);
-
 
     return (
         <div className={"container"}>
