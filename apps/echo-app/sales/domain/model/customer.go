f6770a58b41ae0480f2353ee9940833ce962d579
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-18 11:35:44 +0900

    refactor: 基本型を作る

--- apps/echo-app/sales/domain/model/customer.go
+++ apps/echo-app/sales/domain/model/customer.go
@@ -1,13 +1,17 @@
 package model
 
+type Id int
+type Name string
+type Tel string
+
 type Customer struct {
-	Id   int
-	Name string
-	Tel  string
+	Id   Id
+	Name Name
+	Tel  Tel
 }
 
 func NewCustomer(id int, name string, tel string) (out Customer, err error) {
-	out = Customer{id, name, tel}
+	out = Customer{Id(id), Name(name), Tel(tel)}
 	return
 }
 
