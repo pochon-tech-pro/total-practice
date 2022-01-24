bff4bb46b2eae381beb03dfc17b066ed3d285edc
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-24 12:02:37 +0900

    refactor: any排除

--- apps/nest-app/src/sale/ui/auth/auth.service.ts
+++ apps/nest-app/src/sale/ui/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Staff, StaffsService } from '../staffs/staffs.service';
 import { JwtService } from '@nestjs/jwt';
+import { JWTPayload } from './jwt.strategy';
 
 @Injectable()
 export class AuthService {
@@ -15,6 +16,7 @@ export class AuthService {
   ): Promise<Omit<Staff, 'password'>> {
     const staff = await this.usersService.findOne(name);
     if (staff && staff.password === pass) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = staff;
       return result;
     }
@@ -26,7 +28,7 @@ export class AuthService {
     // subクレームは、JWTのRFC基準に従うために実装しておく必要があります。
     // subクレームは、ユーザーの一意識別子を表現します（値の形式はRFC7519）。基本的にDBのPrimaryKeyでよさそう。
     // https://qiita.com/TakahikoKawasaki/items/8f0e422c7edd2d220e06
-    const payload = { username: staff.name, sub: staff.id };
+    const payload: JWTPayload = { username: staff.name, sub: staff.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
