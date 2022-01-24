6600c424945b4205a1135eb5c98cd79f03140a58
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-24 10:56:59 +0900

    refactor: DI解決ファイルの名前がおかしかったので調整

--- apps/nest-app/src/app.module.ts
+++ apps/nest-app/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CustomerModule } from './sale/customer.module';
+import { SaleModule } from './sale/sale.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './sale/ui/auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppController } from './sale/ui/auth/app.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), CustomerModule, AuthModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(), SaleModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [],
 })
