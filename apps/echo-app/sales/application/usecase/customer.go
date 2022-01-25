34e051067e2d1c09d4c485ba414e1c83ed35a750
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-25 11:21:39 +0900

    feat: 共通部分の構造体は埋め込みで対応する

--- apps/echo-app/sales/application/usecase/customer.go
+++ apps/echo-app/sales/application/usecase/customer.go
@@ -8,21 +8,39 @@ type CustomerUseCase struct {
 	CustomerRepository repository.CustomerRepository
 }
 
-type FindAllOutput struct {
+type Customer struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 	Tel  string `json:"tel"`
+}
+
+type FindAllOutput struct {
+	Customer
 }
 
 func (c CustomerUseCase) FindAll() (out []FindAllOutput, err error) {
 	data := c.CustomerRepository.FindAll()
 	for _, v := range data {
 		out = append(out, FindAllOutput{
-			Id:   v.Id.ToInt(),
-			Name: v.Name.ToString(),
-			Tel:  v.Tel.ToString(),
+			Customer{
+				Id:   v.Id.ToInt(),
+				Name: v.Name.ToString(),
+				Tel:  v.Tel.ToString(),
+			},
 		})
 	}
 	//err = errors.New(fmt.Sprintf("unexpected : %+v", out))
 	return
 }
+
+//
+//type FindOneInput struct {
+//}
+//
+//type FindOneOutput struct {
+//	Id   int    `json:"id"`
+//	Name string `json:"name"`
+//	Tel  string `json:"tel"`
+//}
+//
+//func (c CustomerUseCase) FindOne(in FindOneInput) (out FindOneOutput, err error) {}
